c94221630a7ab5a30a5dab7bae238a8abc938ec2
Jean-Christophe Sanita <jcsanita82@gmail.com>
2022-06-28 10:02:39 +1100

    middle code

--- jeu serpent/script.js
+++ jeu serpent/script.js
@@ -1,58 +1,56 @@
-window.onload = function () 
-{
+window.onload = function () {
   var canvasWidth = 900;
   var canvasHeight = 600;
   var blockSize = 30;
   var ctx;
-  var delay = 100;  
+  var delay = 100;
   var snakee;
 
   init();
 
-  function init() 
-  {
+  function init() {
     var canvas = document.createElement("canvas");
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
     canvas.style.border = "1px solid";
     document.body.appendChild(canvas);
     ctx = canvas.getContext("2d");
-    snakee = new Snake([[6, 4], [5, 4], [4, 4]], "right");
+    snakee = new Snake(
+      [
+        [6, 4],
+        [5, 4],
+        [4, 4],
+      ],
+      "right"
+    );
     refreshCanvas();
-  };
+  }
 
-  function refreshCanvas() 
-  {
+  function refreshCanvas() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     snakee.advance();
     snakee.draw();
     setTimeout(refreshCanvas, delay);
+  }
 
-  };
-
-  function drawBlock(ctx, position) 
-  {
+  function drawBlock(ctx, position) {
     var x = position[0] * blockSize;
     var y = position[1] * blockSize;
     ctx.fillRect(x, y, blockSize, blockSize);
-  };
+  }
 
-  function Snake(body, direction) 
-  {
+  function Snake(body, direction) {
     this.body = body;
     this.direction = direction;
-    this.draw = function ()
-    {
+    this.draw = function () {
       ctx.save();
       ctx.fillStyle = "#ff0000";
-      for (var i = 0; i < this.body.length; i++) 
-      {
+      for (var i = 0; i < this.body.length; i++) {
         drawBlock(ctx, this.body[i]);
       }
       ctx.restore();
     };
-    this.advance = function () 
-    {
+    this.advance = function () {
       var nextPosition = this.body[0].slice();
       switch (this.direction) {
         case "left":
@@ -71,12 +69,10 @@ window.onload = function ()
           throw "invalid direction";
       }
     };
-    
-    this.setDirection = function(newDirection)
-    {
+
+    this.setDirection = function (newDirection) {
       var allowedDirection;
-      switch (this.direction) 
-      {
+      switch (this.direction) {
         case "left":
         case "right":
           allowedDirection = ["up", "down"];
@@ -88,18 +84,15 @@ window.onload = function ()
         default:
           throw "invalid direction";
       }
-      if (allowedDirection.indexOf(newDirection) > -1) 
-      {
+      if (allowedDirection.indexOf(newDirection) > -1) {
         this.newDirection = newDirection;
       }
     };
 
-    document.onkeydown = function handKeyDown(e) 
-    {
+    document.onkeydown = function handKeyDown(e) {
       var key = e.keycode;
       var newDirection;
-      switch (key) 
-      {
+      switch (key) {
         case 37:
           newDirection = "Left";
           break;
@@ -116,17 +109,11 @@ window.onload = function ()
           return;
       }
       snakee.setDirection(newDirection);
-
-    }
-  };
+    };
+  }
 };
 
-
-
-
-
-
-      /*nextPosition[0] += 1;
+/*nextPosition[0] += 1;
       this.body.unshift(nextPosition);
       this.body.pop();
 
@@ -155,4 +142,4 @@ if(isnotBewtweenHorizontalWalls || isnotBewtweenVerticalWalls) {
 
 for (var i=0; i<rest; i++){
     if(snakeX===rest[i][0] && snake)
-}
+}*/
