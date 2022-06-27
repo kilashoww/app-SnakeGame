336ed7d75fac28856608c0451d54078f656e2cbe
Jean-Christophe Sanita <jcsanita82@gmail.com>
2022-06-28 09:34:19 +1100

    reprise 1partCode

--- jeu serpent/script.js
+++ jeu serpent/script.js
@@ -1,57 +1,58 @@
-window.onload = function () {
-  var canvas;
-  var ctx;
-  var delay = 100;
+window.onload = function () 
+{
   var canvasWidth = 900;
   var canvasHeight = 600;
   var blockSize = 30;
+  var ctx;
+  var delay = 100;  
   var snakee;
-console.log("hello")
+
   init();
 
-  function init() {
-    canvas = document.createElement("canvas");
+  function init() 
+  {
+    var canvas = document.createElement("canvas");
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
     canvas.style.border = "1px solid";
     document.body.appendChild(canvas);
     ctx = canvas.getContext("2d");
+    snakee = new Snake([[6, 4], [5, 4], [4, 4]], "right");
     refreshCanvas();
-    snakee = new Snake(
-      [
-        [6, 4],
-        [5, 4],
-        [4, 4],
-      ],
-      "right"
-    );
   }
 
-  function refreshCanvas() {
-    ctx.fillRect(xCoord, yCoord, 100, 50);
-    setTimeout(refreshCanvas, delay);
+  function refreshCanvas() 
+  {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     snakee.advance();
     snakee.draw();
+    setTimeout(refreshCanvas, delay);
+
   }
 
-  function drawBlock(ctx, position) {
+  function drawBlock(ctx, position) 
+  {
     var x = position[0] * blockSize;
     var y = position[1] * blockSize;
     ctx.fillRect(x, y, blockSize, blockSize);
   }
 
-  function Snake(body) {
+  function Snake(body, direction) 
+  {
     this.body = body;
     this.direction = direction;
-    this.draw = function () {
+    this.draw = function ()
+    {
       ctx.save();
       ctx.fillStyle = "#ff0000";
-      for (var i = 0; i < this.body.length; i++) {
+      for (var i = 0; i < this.body.length; i++) 
+      {
         drawBlock(ctx, this.body[i]);
       }
       ctx.restore();
     };
-    this.advance = function () {
+    this.advance = function () 
+    {
       var nextPosition = this.body[0].slice();
       switch (this.direction) {
         case "left":
